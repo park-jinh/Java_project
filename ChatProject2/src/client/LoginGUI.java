1e7b7b024daa50d8ef64aebb61949b2666fe02fc
park-jinh <jinh.drive@gmail.com>
2023-12-01 09:32:10 +0900

    loginGUI 수정

--- ChatProject2/src/client/LoginGUI.java
+++ ChatProject2/src/client/LoginGUI.java
@@ -6,15 +6,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
-import java.awt.event.FocusListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
